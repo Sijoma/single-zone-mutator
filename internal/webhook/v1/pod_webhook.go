e759418afc85931b2bc690e2484e69337c39b61a
Simon Zengerling <simon.zengerling@camunda.com>
2025-08-08 10:53:57 +0200

    init

---
+++ internal/webhook/v1/pod_webhook.go
@@ -0,0 +1,138 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+)
+
+// nolint:unused
+// log is for logging in this package.
+var podlog = logf.Log.WithName("pod-resource")
+
+// SetupPodWebhookWithManager registers the webhook for Pod in the manager.
+func SetupPodWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).For(&corev1.Pod{}).
+		WithDefaulter(&PodCustomDefaulter{client: mgr.GetClient()}).
+		Complete()
+}
+
+// +kubebuilder:rbac:groups="",resources=nodes,verbs=list;get;watch
+// +kubebuilder:webhook:path=/mutate--v1-pod,mutating=true,failurePolicy=ignore,sideEffects=None,groups="",resources=pods,verbs=create;update,versions=v1,name=mpod-v1.kb.io,admissionReviewVersions=v1
+
+// PodCustomDefaulter struct is responsible for setting default values on the custom resource of the
+// Kind Pod when those are created or updated.
+//
+// NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
+// as it is used only for temporary operations and does not need to be deeply copied.
+type PodCustomDefaulter struct {
+	client client.Client
+}
+
+var _ webhook.CustomDefaulter = &PodCustomDefaulter{}
+
+// Default implements webhook.CustomDefaulter so a webhook will be registered for the Kind Pod.
+func (d *PodCustomDefaulter) Default(ctx context.Context, obj runtime.Object) error {
+	pod, ok := obj.(*corev1.Pod)
+
+	if !ok {
+		return fmt.Errorf("expected an Pod object but got %T", obj)
+	}
+	podlog.Info("Defaulting for Pod", "name", pod.GetName())
+
+	if !strings.HasSuffix("-zeebe", pod.GetNamespace()) {
+		podlog.Info("Skipping defaulting for Pod not in zeebe namespace", "namespace", pod.GetNamespace())
+		return nil
+	}
+
+	var nodeList corev1.NodeList
+	if err := d.client.List(ctx, &nodeList); err != nil {
+		podlog.Error(err, "Failed to list nodes for Pod defaulting")
+		return nil
+	}
+
+	zones := []string{}
+	for _, node := range nodeList.Items {
+		if z, ok := node.Labels["topology.kubernetes.io/zone"]; ok {
+			// Check if zone is already in the list to avoid duplicates
+			found := false
+			for _, existingZone := range zones {
+				if existingZone == z {
+					found = true
+					break
+				}
+			}
+			if !found {
+				zones = append(zones, z)
+			}
+		}
+	}
+
+	// If no zones found, use a default
+	if len(zones) == 0 {
+		podlog.Info("No zones found, do nothing")
+		return nil
+	}
+
+	// Get deterministic zone based on namespace
+	zoneIndex := hashString(pod.GetNamespace()) % len(zones)
+	zone := zones[zoneIndex]
+	podlog.Info("Selected zone for namespace", "namespace", pod.GetNamespace(), "zone", zone)
+
+	if pod.Spec.Affinity == nil {
+		pod.Spec.Affinity = &corev1.Affinity{}
+	}
+
+	pod.Spec.Affinity.NodeAffinity = &corev1.NodeAffinity{
+		RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
+			NodeSelectorTerms: []corev1.NodeSelectorTerm{
+				{
+					MatchExpressions: []corev1.NodeSelectorRequirement{
+						{
+							Key:      "topology.kubernetes.io/zone",
+							Operator: corev1.NodeSelectorOpIn,
+							Values:   []string{zone},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return nil
+}
+
+// hashString creates a simple hash from a string to get a deterministic number
+func hashString(s string) int {
+	h := 0
+	for i := 0; i < len(s); i++ {
+		h = 31*h + int(s[i])
+	}
+	if h < 0 {
+		h = -h // Ensure positive number
+	}
+	return h
+}
